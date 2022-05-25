cfa1ea9f428ea60d25854f9cbce4a07492279ffb
Thomas Trividic <thomas.trividic@prestashop.com>
2022-05-25 12:37:33 +0200

    add types for ts

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare namespace PipelineFpUtils {
+    declare function map(g: any): any;
+    declare function mergeObjects(g: any): any;
+    declare function toJsonLine(g: any): any;
+    declare function tap(g: any): any;
+    declare function fromCallback(g: any): any;
+    declare function filter(g: any): any;
+    declare function fromArray(g: any): any;
+}
